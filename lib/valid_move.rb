e16f24fe003415cb6ff1488578a0b273d12a8241
Nur Izdihar Binte Ismail <nurizdiharismail@gmail.com>
2017-09-11 03:12:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if !position_taken?(board, index) && 0 <= index <= 8
+  if !position_taken?(board, index) && 0 <= index && 8 >= index
     return true
   end
 end
